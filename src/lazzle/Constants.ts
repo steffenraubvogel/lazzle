d3bcc6c24d2cdd94b397fceba7d605ccf403d37b
Steffen Harbich <steffen@raubvogel.net>
2021-01-06 15:38:08 +0100

    hints (arrows) for rotating laser, moved laser styles to own scss module

--- src/lazzle/Constants.ts
+++ src/lazzle/Constants.ts
@@ -1,6 +1,6 @@
 // visuals
 export const WORLD_WIDTH: number = 850
-export const WORLD_HEIGHT: number = 450
+export const WORLD_HEIGHT: number = 425
 export const BLOCK_SIZE: number = 32
 
 // local storage keys
--- src/lazzle/Laser.tsx
+++ src/lazzle/Laser.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "./Lazzle.module.scss";
+import styles from "./Laser.module.scss";
 import { LaserShotPhase, Phase, SetupPhase } from "./Phase";
 import { LevelLaser } from "./Levels";
 import { nextId } from "./Util";
@@ -8,6 +8,7 @@ import { Block } from "./Block";
 import { Point, rayIntersectsBlock } from "./Geometry";
 import { ReactComponent as GripIcon } from "bootstrap-icons/icons/grip-vertical.svg"
 import { ReactComponent as ArrowRightIcon } from "bootstrap-icons/icons/arrow-right.svg"
+import { ReactComponent as ReplyIcon } from "bootstrap-icons/icons/reply.svg"
 
 export class Laser {
 
@@ -24,6 +25,11 @@ export class Laser {
     private _rightMoveArrowPos: Point = { x: 0, y: 0 } // moving indicator position right of laser
     private _rightMoveArrowRotation: number = 0
 
+    private _leftRotationArrowPos: Point = { x: 0, y: 0}
+    private _leftRotationArrowRotation: number = 0
+    private _rightRotationArrowPos: Point = { x: 0, y: 0}
+    private _rightRotationArrowRotation: number = 0
+
     constructor(config: LevelLaser) {
         this._angle = config.angle * Math.PI / 180
         this._distance = config.distance
@@ -38,17 +44,35 @@ export class Laser {
         this._x = WORLD_WIDTH / 2 + this._distance * Math.cos(this._angle)
         this._y = WORLD_HEIGHT - this._distance * Math.sin(this._angle)
 
+        const MOVE_ARROW_ANGLE_GAP = 8 * Math.PI / 180 // in rad
+
         this._leftMoveArrowPos = {
-            x: WORLD_WIDTH / 2 + Math.cos(this._angle + 8 * Math.PI / 180) * this._distance,
-            y: WORLD_HEIGHT - Math.sin(this._angle + 8 * Math.PI / 180) * this._distance
+            x: WORLD_WIDTH / 2 + Math.cos(this._angle + MOVE_ARROW_ANGLE_GAP) * this._distance,
+            y: WORLD_HEIGHT - Math.sin(this._angle + MOVE_ARROW_ANGLE_GAP) * this._distance
         }
-        this._leftMoveArrowRotation = -this._angle - 8 * Math.PI / 180 - Math.PI / 2
+        this._leftMoveArrowRotation = -this._angle - MOVE_ARROW_ANGLE_GAP - Math.PI / 2
 
         this._rightMoveArrowPos = {
-            x: WORLD_WIDTH / 2 + Math.cos(this._angle - 8 * Math.PI / 180) * this._distance,
-            y: WORLD_HEIGHT - Math.sin(this._angle - 8 * Math.PI / 180) * this._distance
+            x: WORLD_WIDTH / 2 + Math.cos(this._angle - MOVE_ARROW_ANGLE_GAP) * this._distance,
+            y: WORLD_HEIGHT - Math.sin(this._angle - MOVE_ARROW_ANGLE_GAP) * this._distance
         }
-        this._rightMoveArrowRotation = -this._angle + 8 * Math.PI / 180 + Math.PI / 2
+        this._rightMoveArrowRotation = -this._angle + MOVE_ARROW_ANGLE_GAP + Math.PI / 2
+
+        const ROTATION_ARROW_DISTANCE_GAP = 80 // in px
+        const ROTATION_ARROW_ANGLE_GAP = 25 * Math.PI / 180 // in rad
+        const ROTATION_ARROW_ROTATION_CORRECTION = 45 * Math.PI / 180 // in rad
+
+        this._leftRotationArrowPos = {
+            x: this._x + Math.cos(this._rotation - ROTATION_ARROW_ANGLE_GAP) * ROTATION_ARROW_DISTANCE_GAP,
+            y: this._y + Math.sin(this._rotation - ROTATION_ARROW_ANGLE_GAP) * ROTATION_ARROW_DISTANCE_GAP,
+        }
+        this._leftRotationArrowRotation = this._rotation + Math.PI / 2 - ROTATION_ARROW_ROTATION_CORRECTION
+
+        this._rightRotationArrowPos = {
+            x: this._x + Math.cos(this._rotation + ROTATION_ARROW_ANGLE_GAP) * ROTATION_ARROW_DISTANCE_GAP,
+            y: this._y + Math.sin(this._rotation + ROTATION_ARROW_ANGLE_GAP) * ROTATION_ARROW_DISTANCE_GAP,
+        }
+        this._rightRotationArrowRotation = this._rotation + Math.PI / 2 + ROTATION_ARROW_ROTATION_CORRECTION
     }
 
     public drag(mouseX: number, mouseY: number) {
@@ -66,6 +90,7 @@ export class Laser {
         const relativeX = this._x - mouseX
         const relativeY = this._y - mouseY
         this._rotation = Math.atan2(relativeY, relativeX) - Math.PI
+        this.updatePosition()
     }
 
     get x() {
@@ -102,6 +127,22 @@ export class Laser {
 
     get rightMoveArrowRotation() {
         return this._rightMoveArrowRotation
+    }
+
+    get leftRotationArrowPos() {
+        return this._leftRotationArrowPos
+    }
+
+    get leftRotationArrowRotation() {
+        return this._leftRotationArrowRotation
+    }
+
+    get rightRotationArrowPos() {
+        return this._rightRotationArrowPos
+    }
+
+    get rightRotationArrowRotation() {
+        return this._rightRotationArrowRotation
     }
 }
 
@@ -199,6 +240,20 @@ export default function LaserComponent(props: { laser: Laser, blocks: Block[], p
                 transform: 'translate(-50%, -50%) rotate(' + props.laser.rightMoveArrowRotation + 'rad)'
             }}>
                 <ArrowRightIcon />
+            </div>
+            <div className={styles.laserRotationHandleArrow} style={{
+                left: props.laser.leftRotationArrowPos.x,
+                top: props.laser.leftRotationArrowPos.y,
+                transform: 'translate(-50%, -50%) rotate(' + props.laser.leftRotationArrowRotation + 'rad) scaleX(-1)'
+            }}>
+                <ReplyIcon />
+            </div>
+            <div className={styles.laserRotationHandleArrow} style={{
+                left: props.laser.rightRotationArrowPos.x,
+                top: props.laser.rightRotationArrowPos.y,
+                transform: 'translate(-50%, -50%) rotate(' + props.laser.rightRotationArrowRotation + 'rad)'
+            }}>
+                <ReplyIcon />
             </div>
             <div className={styles.laserHitBlocks}>
                 {props.blocks.filter(b => b.state !== "destroyed")
